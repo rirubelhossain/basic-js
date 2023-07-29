3d0e8d2085d88a570051ee334adf9b9a8fff4c9f
rirubelhossain <rubelphp94@gmail.com>
2023-07-29 23:26:58 +0600

    simple js structure

--- test.js
+++ test.js
@@ -3,4 +3,21 @@ console.log(34)
 console.log(17%5)
 
 
+let a , b ,c , result;
 
+a = 10 ;
+b = 20 ;
+c = 5 ;
+result = a + b + c ;
+console.log(result);
+
+var number1 , number2 ;
+number1 = 100 ;
+number2 = 200 ;
+
+var number_result = number1 + number2 ;
+console.log(number_result);
+
+const cars = [ "Saab" , "Volvo" , "BMW"] ;
+cars.push("Rabbani");
+console.log(cars[4]);
