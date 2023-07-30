bcf26be59a5fc1b094210bd1f7f375f44610f362
rirubelhossain <rubelphp94@gmail.com>
2023-07-31 00:13:25 +0600

    loop in js

--- test.js
+++ test.js
@@ -38,13 +38,32 @@
 // console.log(person.firstName) ;
 // console.log(person.lastName) ;
 
-const object = {
-    a : 10 ,
-    b : 20 ,
-    c : 40 ,
-    calculate : function(){
-        return this.a + this.b + this.c ;
-    }
-};
-console.log(object.calculate());
+// const object = {
+//     a : 10 ,
+//     b : 20 ,
+//     c : 40 ,
+//     calculate : function(){
+//         return this.a + this.b + this.c ;
+//     }
+// };
+// console.log(object.calculate());
 
+// const simple_calculate = {
+//     cal : function(number1 , number2){
+//         return number1 + number2 ;
+//     }
+// };
+// console.log(cal(12,3));
+
+
+ 
+console.log(6 != 6)
+
+var i = 1 ;
+var sum = 0 ;
+while( i <= 100 ){
+    sum += i ;
+    console.log(i);
+    ++i ;
+}
+console.log(sum);
