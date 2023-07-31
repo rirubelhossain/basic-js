ce82462e7f794bef114b238a27343eb141a158e6
rirubelhossain <rubelphp94@gmail.com>
2023-07-31 22:00:14 +0600

    comparison operator

---
+++ basic.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+var a , b , c ;
+a = 10 ;
+b = 30 ; 
+c = 40 ;
+if( a > b ){
+    if( a > c ){
+        console.log("A is grater");
+    }else{
+        console.log("B is grater");
+    }
+}else if( b > c ){
+    console.log("B is grater");
+}else{
+    console.log("C is grater");
+}
