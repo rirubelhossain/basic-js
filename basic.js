41ce05512e5c42e676ea790af3c683e228a23920
rirubelhossain <rubelphp94@gmail.com>
2023-08-01 23:06:07 +0600

    infinity loop

--- basic.js
+++ basic.js
@@ -14,3 +14,26 @@ if( a > b ){
 }else{
     console.log("C is grater");
 }
+
+var ar = [1,2,3,4];
+var sum = 0 ;
+var max = ar[0];
+for(var i = 0  ; i < ar.length ; ++i )
+{   sum += ar[i];
+
+    if( max >= ar[i]){
+        max = ar[i];
+    }
+    console.log(ar[i]);
+}
+console.log("The total summation is = " + sum );
+
+console.log(max);
+
+var nm = "false";
+console.log(typeof nm)
+
+var k = 5 ;
+for(  k < 50 ; ++k ){
+    console.log(k);
+}
