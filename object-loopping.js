d1e4d294800595f4f460e4d7b56c59743931f700
rirubelhossain <rubelphp94@gmail.com>
2023-08-02 02:30:10 +0600

    switch loop

--- object-loopping.js
+++ object-loopping.js
@@ -14,3 +14,18 @@ console.log(values)
 for( var i = 0 ; i < keys.length ; ++i ){
     console.log(keys[i] + " = " + values[i]);
 }
+
+var test = 212 ;
+switch(test){
+    case 1 :
+    console.log("1") 
+    break ;
+    case 32:
+        console.log("32")
+        break
+    case 21:
+        console.log("got the value")
+        break 
+    default:
+    console.log("No matches")
+}
