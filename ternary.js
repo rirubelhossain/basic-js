12ffec91ab5512eb5d2b8b2f1dc0620a79abeff9
rirubelhossain <rubelphp94@gmail.com>
2023-08-04 00:49:29 +0600

    Function expression

---
+++ ternary.js
@@ -0,0 +1,26 @@
+
+// var number 
+var number = 9;
+
+number >= 0 ? console.log("Positive") : console.log("Negative") 
+
+
+(function squre()
+{
+    var a , b ;
+    a = 20 ;
+    b = 3 ;
+    // document.write("<br>The result is = " + (a*b) + "<br>");
+    console.log("Hello")
+})();
+
+// squre()
+/// this is called function expression it's really nice 
+const fun = function add(){
+    var number1 , number2;
+    number1 = 10 ;
+    number2 = 30 ;
+    var result = number1 + number2 ;
+    console.log("The result of the function = ", result);
+}
+fun();
