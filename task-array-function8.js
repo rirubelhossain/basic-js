7b6dff08c60f92c4db48a2f1b1c7b02ac9645a8e
rirubelhossain <rubelphp94@gmail.com>
2023-08-04 02:28:30 +0600

    Max number and name in two dimensional array

--- task-array-function8.js
+++ task-array-function8.js
@@ -18,3 +18,34 @@ function highestScore(ar){
 var ar = [10,200,3,40];
 var result = highestScore(ar);
 console.log(result);
+
+
+
+// How to get maximum number and name from two dimensional array
+
+
+function GetHightestScore(towAr){
+    var tt = towAr[0][1] ;
+    var name1 ;
+    console.log(tt)
+    for(var k = 0 ; k < towAr.length ; ++k){
+        for(var j = 0 ; j < towAr.length ; ++j ){
+            if( towAr[k][j] > tt ){
+                tt = towAr[k][j];
+                name1 = towAr[k][0] 
+            }
+        }
+        // console.log("The max number is = ", tt, " " , name1);
+    }
+    return name1 ;
+}
+var towAr = [
+    ["rubel",20],
+    ["Ash",30],
+    ["Sab",400],
+    ["Mushi", 50],
+    ["Sakib", 500]
+]
+console.log("The name of the max runs scorers in the array is ", GetHightestScore(towAr));
+
+// console.log(towAr)
