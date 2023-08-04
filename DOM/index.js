1a6217836326c3f86bf614e729264d99c7488cfd
rirubelhossain <rubelphp94@gmail.com>
2023-08-04 11:09:22 +0600

    how to use query selector

--- DOM/index.js
+++ DOM/index.js
@@ -1,6 +1,8 @@
 
-var myheading = document.getElementById("heading") ;
-myheading.innerHTML = "Hello Javascript, this is dom manipulation";
+ 
 
-document.getElementById("pera").innerHTML = "This is paragraph"
-document.getElementById("heading2").innerHTML = "Goodbye <br> Goodbye"
+// document.getElementById("pera").innerHTML = "This is paragraph"
+// document.getElementById("heading2").innerHTML = "Goodbye <br> Goodbye"
+
+
+document.querySelector(".para").innerHTML = "Hello query";
