0a73123e3941281ceda1e676e2dee4ff24abe903
rirubelhossain <rubelphp94@gmail.com>
2023-08-04 10:58:04 +0600

    DOM manipulation

---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+
+var myheading = document.getElementById("heading") ;
+myheading.innerHTML = "Hello Javascript, this is dom manipulation";
+
+document.getElementById("pera").innerHTML = "This is paragraph"
+document.getElementById("heading2").innerHTML = "Goodbye <br> Goodbye"
