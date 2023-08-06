3e09787c7ea97593c5c454d5ce293b41f1d3a0a2
rirubelhossain <rubelphp94@gmail.com>
2023-08-06 09:21:31 +0600

    string

---
+++ string.js
@@ -0,0 +1,13 @@
+const test = "hello man how are you?";
+let check = test.includes("hello");
+console.log(check);
+
+
+// how i'm going to use toLower function
+
+
+const test1 = "Hello man how are you? yesterday you did not take my phone call, you don't have any idea that i was in involved an important task" ;
+
+const jam = test1.toUpperCase();
+
+console.log(jam)
