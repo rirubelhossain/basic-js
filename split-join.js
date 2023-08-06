fffce6e9225dc0e149f5d41fded6e9b05af470a4
rirubelhossain <rubelphp94@gmail.com>
2023-08-06 11:19:04 +0600

    how to use split() function that make divide using the condition in the function

---
+++ split-join.js
@@ -0,0 +1,8 @@
+const lyrics = "tumi bondhu kala pakhi ami jeno ki, boshonto kale tomy bolte pari ni. kala kala sada kala rong legeche sada kala.";
+
+console.log(lyrics)
+const parts = lyrics.split(' ');
+console.log(parts)
+
+const sentences  = lyrics.split('.')
+console.log(sentences)
