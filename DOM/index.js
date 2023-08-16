bbe93bb9255465c51cf7cf03bd0ff8ffd9ca605d
rirubelhossain <rubelphp94@gmail.com>
2023-08-16 20:53:14 +0600

    set color for background

--- DOM/index.js
+++ DOM/index.js
@@ -6,5 +6,16 @@
 
 
 // document.querySelector(".para").innerHTML = "Hello query";
-const useQuery = document.querySelector("h1");
-useQuery.style.background="yellow"
+// const useQuery = document.querySelector("h1");
+// useQuery.style.background="yellow"
+
+
+function fun_red(){
+    // const useQuery_1 = document.getElementById("h11");
+    // useQuery_1.body.background  = "red";
+    document.body.style.backgroundColor = "red";
+}
+function fun_yellow()
+{
+    document.body.style.backgroundColor = "yellow";
+}
